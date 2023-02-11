b43c3a26fdde21c78e153bcc25820a9dbf2b50e5
lachose-jB <jadjahouisso@gmail.com>
2023-02-11 22:40:25 +0100

    1er integration

--- src/Composant/TodoList/TodoItem/Todo.js
+++ src/Composant/TodoList/TodoItem/Todo.js
@@ -1,13 +1,13 @@
 import { checkPropTypes } from "prop-types";
-const Todo = () => {
+const Todo = (id, title) => {
 
   return ( 
-    <div>
+    <section className="main">
         <input id="toggle-all" className="toggle-all" type="checkbox" />
-        <label for="toggle-all">Mark all as complete</label>
+        <label htmlFor="toggle-all">Mark all as complete</label>
         <ul className="todo-list">
-        {/<!-- These are here just to show the structure of the list items -->/}
-					{/<!-- List items should get the class `editing` when editing and `completed` when marked as completed -->/}
+            {/*<!-- These are here just to show the structure of the list items -->*/}
+            {/*<!-- List items should get the className `editing` when editing and `completed` when marked as completed -->*/}
             <li className="completed">
                 <div className="view">
                     <input className="toggle" type="checkbox" checked />
@@ -25,7 +25,7 @@ const Todo = () => {
                 <input className="edit" value="Rule the web" />
             </li>
         </ul>
-	</div>
+    </section>
    );
 }
 
--- src/Composant/TodoList/Todolist/TodoList.js
+++ src/Composant/TodoList/Todolist/TodoList.js
@@ -1,35 +1,36 @@
 import Todo from "../TodoItem/Todo"
 import { checkPropTypes } from "prop-types";
-const TodoList = () => {
+const TodoList = ({todo}) => {
+	
 
     return ( 
-      <div>
-        <section className="todoapp">
+      <section className="todoapp">
 			<header className="header">
 				<h1>todos</h1>
-				<input className="new-todo" placeholder="What needs to be done?" autofocus />
+				<input className="new-todo" placeholder="What needs to be done?"/>
 			</header>
-			{/<!-- This section should be hidden by default and shown when there are todos -->/}
-			
-			{/<!-- This footer should hidden by default and shown when there are todos -->/}
-			<footer className="footer">
-				{/<!-- This should be `0 items left` by default -->/}
-				<span className="todo-count"><strong>0</strong> item left</span>
-				{/<!-- Remove this if you don't implement routing -->/}
+			{/*<!-- This section should be hidden by default and shown when there are todos -->*/}
 				<Todo/>
-				{/<!-- Hidden if no completed items are left ↓ -->/}
+			{/*<!-- This footer should hidden by default and shown when there are todos -->*/}
+			<footer className="footer">
+				{/*<!-- This should be `0 items left` by default -->*/}
+				<span className="todo-count"><strong>0</strong> item left</span>
+				{/*<!-- Remove this if you don't implement routing -->*/}
+				<ul className="filters">
+					<li>
+						<a className="selected" href="#/">All</a>
+					</li>
+					<li>
+						<a href="#/active">Active</a>
+					</li>
+					<li>
+						<a href="#/completed">Completed</a>
+					</li>
+				</ul>
+				{/*<!-- Hidden if no completed items are left ↓ -->*/}
 				<button className="clear-completed">Clear completed (1)</button>
 			</footer>
 		</section>
-		<footer className="info">
-			<p>Double-click to edit a todo</p>
-			{/<!-- Remove the below line ↓ -->/}
-			<p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
-			{/<!-- Change this out with your name and url ↓ -->/}
-			<p>Created by <a href="http://todomvc.com">you</a></p>
-			<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-		</footer>
-      </div>
      );
   }
   
